69d557682f80ebcc2ab2d0b2ccd0708cf301d54b
Veera Ihalainen <Veera.helli@gmail.com>
2022-05-25 15:24:37 +0300

    1.1 done

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -1,8 +1,60 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.p1} {props.e1}
+      </p>
+      <p>
+        {props.p2} {props.e2}
+      </p>
+      <p>
+        {props.p3} {props.e3}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.a + props.b + props.c}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+
+      <Content
+        p1={part1}
+        e1={exercises1}
+        p2={part2}
+        e2={exercises2}
+        p3={part3}
+        e3={exercises3}
+      />
+
+      <Total a={exercises1} b={exercises2} c={exercises3} />
+    </div>
+  );
+};
 
 export default App;
-//Test
