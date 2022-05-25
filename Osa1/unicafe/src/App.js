e953a5cf1c0a198fcd65f8c669307439ceca755f
Veera Ihalainen <Veera.helli@gmail.com>
2022-05-26 01:48:09 +0300

    1.9 done

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -1,7 +1,30 @@
 import { useState } from "react";
 
 const Statistics = (props) => {
-  // ...
+  const good = props.good;
+  const neutral = props.neutral;
+  const bad = props.bad;
+  const total = good + neutral + bad;
+
+  if (good > 0 || bad > 0 || neutral > 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+        <p>Average {(good + bad * -1) / total}</p>
+        <p>Positive {(good / total) * 100} %</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
 };
 
 function App() {
@@ -15,10 +38,8 @@ function App() {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
