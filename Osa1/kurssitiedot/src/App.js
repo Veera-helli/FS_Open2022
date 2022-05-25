7591b6cb56cab0acd024ecd72dc352a3f7b8ec12
Veera Ihalainen <Veera.helli@gmail.com>
2022-05-25 15:53:03 +0300

    1.4 done

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -36,31 +36,37 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
       <Content
-        p1={part1.name}
-        e1={part1.exercises}
-        p2={part2.name}
-        e2={part2.exercises}
-        p3={part3.name}
-        e3={part3.exercises}
+        p1={parts[0].name}
+        e1={parts[0].exercises}
+        p2={parts[1].name}
+        e2={parts[1].exercises}
+        p3={parts[2].name}
+        e3={parts[2].exercises}
       />
-      <Total a={part1.exercises} b={part2.exercises} c={part3.exercises} />
+      <Total
+        a={parts[0].exercises}
+        b={parts[1].exercises}
+        c={parts[2].exercises}
+      />
     </div>
   );
 };
