cbeba93314554fcf22f4936051d05036b41348a8
Veera Ihalainen <Veera.helli@gmail.com>
2022-05-30 10:17:32 +0300

    2.7 done

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Person = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{props.person.name}</p>
     </div>
   );
-}
+};
+//alert("Hello world!");
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    //console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addName = (event) => {
+    event.preventDefault();
+
+    if (persons.find((p) => p.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      setNewName("");
+    } else {
+      const nameObject = {
+        name: newName,
+        id: persons.length + 1,
+      };
+      setPersons(persons.concat(nameObject));
+      setNewName("");
+      console.log("new name added");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <Person person={person} key={person.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
