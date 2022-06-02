3a5c43d76b2236791b1ab6c6c5314d57b1757815
Veera Ihalainen <Veera.helli@gmail.com>
2022-06-02 13:36:19 +0300

    refactoring

--- Osa2/maiden_tiedot/src/App.js
+++ Osa2/maiden_tiedot/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Filter from "./Filter";
+import Filter from "./components/Filter";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
