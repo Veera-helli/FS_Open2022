fabaa713b3f39102144709d107c5e70b1aca7d04
Veera Ihalainen <Veera.helli@gmail.com>
2022-06-06 14:59:54 +0300

    4.5 done

--- Osa4/bloglist/utils/list_helper.js
+++ Osa4/bloglist/utils/list_helper.js
@@ -10,7 +10,17 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+//favoriteBlog
+const favoriteBlog = (blogs) => {
+  const reducer = (favorite, item) => {
+    return favorite.likes >= item.likes ? favorite : item;
+  };
+
+  return blogs.length === 0 ? NaN : blogs.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
