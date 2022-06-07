74d7b39a0c20a180023037f25766d0bc9d30b599
Veera Ihalainen <Veera.helli@gmail.com>
2022-06-07 09:45:34 +0300

    4.6 done

--- Osa4/bloglist/utils/list_helper.js
+++ Osa4/bloglist/utils/list_helper.js
@@ -1,3 +1,7 @@
+// Requiring the lodash library
+const lodash = require('lodash');
+
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1;
 };
@@ -19,8 +23,27 @@ const favoriteBlog = (blogs) => {
   return blogs.length === 0 ? NaN : blogs.reduce(reducer, 0);
 };
 
+const mostBlogs = (bloglist) => {
+  const bloggerObj = lodash.countBy(bloglist, 'author');
+  const reducer = (mostBlogger, item) => {
+    return mostBlogger.blogs >= item.blogs ? mostBlogger : item;
+  };
+
+  const ret = Object.entries(bloggerObj)
+    .map((p) => ({
+      author: p[0],
+      blogs: p[1],
+    }))
+    .reduce(reducer, {});
+
+  //console.log(`length ${bloglist.length}: ${JSON.stringify(ret)}`);
+
+  return ret;
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
