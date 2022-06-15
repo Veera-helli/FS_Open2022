9bfc65673f6259b003729bc1927ddaf4efaf9224
Veera Ihalainen <Veera.helli@gmail.com>
2022-06-15 10:31:52 +0300

    4.19 done

--- Osa4/bloglist/controllers/blogs.js
+++ Osa4/bloglist/controllers/blogs.js
@@ -1,6 +1,17 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
+require('dotenv').config();
+
+const getTokenFrom = (request) => {
+  console.log('Getting token');
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -9,8 +20,15 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body;
-  const userArr = await User.find({});
-  const user = userArr[0];
+
+  const token = getTokenFrom(request);
+  console.log('Got token');
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  console.log(decodedToken.id);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
